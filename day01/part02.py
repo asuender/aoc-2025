43079577eb7442e73e4d12ef596fdc0e4d979fe8
Andreas Sünder <andreas.suender@icloud.com>
2025-12-01 19:24:10 +0100

    Solve day 1 part 2

---
+++ day01/part02.py
@@ -0,0 +1,33 @@
+import os
+
+DATA_PATH = os.path.join(os.path.dirname(__file__), "data.txt")
+
+def zeros_during_move(start_dial: int, dir: str, by: int):
+    sign = -1 if dir == "L" else 1
+
+    k0 = (-sign * start_dial) % 100
+    first_zero = 100 if k0 == 0 else k0
+
+    if first_zero > by:
+        return 0
+
+    return 1 + (by - first_zero) // 100
+
+def solve():
+    dial = 50
+    count_dial_zero = 0
+
+    with open(DATA_PATH, "r") as file:
+        for line in file.readlines():
+            dir = line[0]
+            by = int(line[1:])
+
+            count_dial_zero += zeros_during_move(dial, dir, by)
+
+            sign = -1 if dir == "L" else 1
+            dial = (dial + sign * by) % 100
+
+    return count_dial_zero
+
+if __name__ == "__main__":
+    print(solve())
